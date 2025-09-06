5cc86e0290af220cb38f1fa0950ec8760749d19c
unknown <Ifemi18100@gmail.com>
2025-09-06 09:28:44 -0700

    Add resume PDF and update Home component styles and logic

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -17,14 +17,14 @@ function Home(){
             <div className='content ' >
                 <div className='left '>
                     <h1>AYINDE YUSUF IFEMI</h1>
-                    <h2>Frontend Web Developer</h2>
-                    <p> I focused on meeting deadlines and delivering</p>
-                    <p>results — confident working solo or with a team.</p>
+                    <h2 className='text-[20px]'>Frontend Web Developer</h2>
+                    <p className='text-[20px]'> I focused on meeting deadlines and delivering</p>
+                    <p className='text-[17px]'>results — confident working solo or with a team.</p>
 
                 </div>
                 <div className='right'>
                     <Link to='Mystack' smooth={true} duration={500}><button className='btn text-white bg-red-700 p-[5px] m-[5px] '>Check Stacks</button></Link> <br/>
-                    <button className='btn text-white bg-red-700 p-[5px] m-[5px]'>Download Resume</button>
+                    <a href="/Ayinde_Yusuff_resume.pdf" download="AYINDE Yusuf Resume"><button className='btn text-white bg-red-700 p-[5px] m-[5px]'>Download Resume</button></a>
                 </div>
 
             </div>
