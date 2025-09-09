48ed87ec33752a5e84287a8ece0e6175fce37bca
unknown <Ifemi18100@gmail.com>
2025-09-09 12:37:49 -0700

    added little magi

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -20,12 +20,11 @@ function Home(){
                     <h2 className='text-[20px]'>Frontend Web Developer</h2>
                     <p className='text-[20px]'> I focused on meeting deadlines and delivering</p>
                     <p className='text-[17px]'>results — confident working solo or with a team.</p>
+                     <Link to='Mystack' smooth={true} duration={500}><button className='btn text-white bg-red-700 p-[5px] m-[5px] '>Check Stacks</button></Link> <br/>
+                    <a href="/Ayinde_Yusuff_resume.pdf" download="AYINDE Yusuf Resume"><button className='btn text-white bg-red-700 p-[5px] m-[5px]'>Download Resume</button></a>
 
                 </div>
-                <div className='right'>
-                    <Link to='Mystack' smooth={true} duration={500}><button className='btn text-white bg-red-700 p-[5px] m-[5px] '>Check Stacks</button></Link> <br/>
-                    <a href="/Ayinde_Yusuff_resume.pdf" download="AYINDE Yusuf Resume"><button className='btn text-white bg-red-700 p-[5px] m-[5px]'>Download Resume</button></a>
-                </div>
+                
 
             </div>
         
